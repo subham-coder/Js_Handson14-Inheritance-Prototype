59b0cfed41885d7103333c409c986987bf92e28b
subham-coder <61961073+subham-coder@users.noreply.github.com>
2023-04-09 11:09:07 +0530

    uploaded

---
+++ script.js
@@ -0,0 +1,43 @@
+// Q1..
+// const parent = {
+//     fname : "Prepbytes",
+//     lname: "EA21",
+//     fullname : function abc() {
+//        console.log(this.fname + " " + this.lname);
+//     }
+// }
+// const child = {
+//     _proto_: parent
+// }
+// child.fullname();
+
+// Q2..
+// const obj1 = {
+//     name: "Prepbytes"
+// }
+
+// const obj2 = {
+//     _proto_: obj1,
+//     lname:"EA21",
+// }
+// const obj3 = {
+//     _proto_: obj2,
+//     age: "20",
+// }
+// console.log(obj3.name);
+
+// Q3..
+// const Add = {
+//     sum : function(arr) {
+//         let result= 0;
+//         for(let i=0; i< arr.length; i++) {
+//             result+= arr[i];
+//         }
+//         console.log(result);
+//     }
+
+// }
+// Add.sum([1,2,3,4,5])
+
+// Q3..
+
